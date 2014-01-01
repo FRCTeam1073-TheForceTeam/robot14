9ad1573956c680f4033aa193c05eaf921e4edfce
Evin Ugur <evinoog96@gmail.com>
2014-01-01 18:18:07 -0500

    Made an empty repo, see you all on Saturday :)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 robot14
 =======
+
+In the PowerShell run the file `setup.bat` to get the a complete codebase once you have the code cloned to your computer
---
+++ setup.bat
@@ -0,0 +1,4 @@
+rem on a fresh clone this'll setup the repository
+git submodule init 
+rem this will pull down WPILibExtensions code as needed
+git submodule update
