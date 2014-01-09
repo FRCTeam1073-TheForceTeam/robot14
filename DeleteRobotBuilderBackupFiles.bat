2edb8153db6cee9f0fae8ec29e4b7d2b1dcab481
Evin Ugur <evinoog96@gmail.com>
2014-01-09 10:47:05 -0500

    Added Script to clean up RB, Changed Matt W's list to MD for hyperlinks

---
+++ DeleteRobotBuilderBackupFiles.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+ECHO Deleting Robot Builder temporary files
+CD ..
+DEL /S *.h~
+DEL /S *.cpp~
+DEL /S *.yml~
+DEL /S *.html~
+DEL /S *.wrproject~
+DEL /S *.cproject~
+DEL /S *.project~
+DEL /S *.wrmakefile~
+DEL ~botBuilderConfiguration.yml
+ECHO Robot Builder temporary files have been deleted
