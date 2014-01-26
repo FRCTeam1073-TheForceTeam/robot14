705413b74cf798b22fc6542a57f85b2ee20c43e2
TechnoD11 <technoD@charter.net>
2014-01-26 13:57:51 -0500

    Working Autonomous
    In robotmap, set boolean to false to enable autonomous
    values for specific positions are configured for the mobile base, not
    the final robot.

---
+++ Commands/AutonomousCommmandGroup.cpp
@@ -0,0 +1,30 @@
+/* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
+Do not mix this code with any other version of RobotBuilder! */
+
+
+
+#include "AutonomousCommmandGroup.h"
+#include "AutonomousDriveCommand.h"
+#include "WaitForHotGoal.h"
+#include "AutonomousLaunchCommand.h"
+AutonomousCommmandGroup::AutonomousCommmandGroup() {
+	AddSequential(new AutonomousDriveCommand());
+	AddSequential(new WaitForHotGoal());
+	AddSequential(new AutonomousLaunchCommand());
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+}
---
+++ Commands/AutonomousCommmandGroup.h
@@ -0,0 +1,21 @@
+/* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
+Do not mix this code with any other version of RobotBuilder! */
+
+
+
+#ifndef AUTONOMOUSCOMMMANDGROUP_H
+#define AUTONOMOUSCOMMMANDGROUP_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class AutonomousCommmandGroup: public CommandGroup {
+public:	
+	AutonomousCommmandGroup();
+};
+
+#endif
