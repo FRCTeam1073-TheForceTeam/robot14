a124103bf64016b10599958299cb379e33770a7b
Matt Thyng <mattthyng@gmail.com>
2014-02-01 15:23:17 -0500

    Added Shooter Ready Box stuff
    still needs ultra sonic sensor code

--- Subsystems/Dashboard14.cpp
+++ Subsystems/Dashboard14.cpp
@@ -1,6 +1,8 @@
 /* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
 Do not mix this code with any other version of RobotBuilder! */
 #include "Dashboard14.h"
+#include "../Robot.h"
+#include "../Robot.cpp"
 #include "../Robotmap.h"
 #include "../Commands/SendToDashboard.h"
 Dashboard14::Dashboard14() : Subsystem("Dashboard14") {
@@ -22,11 +24,26 @@ void Dashboard14::SendData()
 	SmartDashboard::PutNumber("Battery", DriverStation::GetInstance()->GetBatteryVoltage());
 	SmartDashboard::PutNumber("TimeCounterWidget", DriverStation::GetInstance()->GetMatchTime());
 	// this code doesn't do anything
-	DriverStation::GetInstance()->IsAutonomous();
-	DriverStation::GetInstance()->IsDisabled();
 	// the code starts doing stuff again here
-	//SmartDashboard::PutNumber("PSI Gauge", Robot::airCompressor->GetPressurePSI);
+	SmartDashboard::PutNumber("PSI Gauge", Robot::AirCompressor::GetPressurePSI);
 	SmartDashboard::PutBoolean("Gear Shifter", Robot::shifter->IsLowGear());
+	
+	if(Robot::AirCompressor::GetPressurePSI > 60 /* && good distance*/)
+	{
+		SmartDashboard::PutNumber("Shooter Ready", 1);
+	}
+	else if(Robot::AirCompressor::GetPressurePSI > 60 /* && !good distance*/)
+	{
+		SmartDashboard::PutNumber("Shooter Ready", 0);
+	}
+	else if(Robot::AirCompressor::GetPressurePSI < 60 /* && good distance*/)
+	{
+		SmartDashboard::PutNumber("Shooter Ready", -1);
+	}
+	else
+	{
+		SmartDashboard::PutNumber("Shooter Ready", -2);
+	}
 	
 	
 }
