23a1e54bd97c4c4cc83ce73b2e24e87bbbd64a0a
Jeremy Quinn <jeremy.q97@gmail.com>
2014-02-08 17:38:58 -0500

    Elevator PID stuff, needs testing

--- Commands/ElevateCollector.cpp
+++ Commands/ElevateCollector.cpp
@@ -1,19 +1,24 @@
 /* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
 Do not mix this code with any other version of RobotBuilder! */
 #include "ElevateCollector.h"
-ElevateCollector::ElevateCollector(bool dir) {
-	Construct(dir);
+ElevateCollector::ElevateCollector(bool up) {
+	Construct(up);
 }
 ElevateCollector::ElevateCollector(){
 	Construct(true);
 }
-void ElevateCollector::Construct(bool dir){
-	this->dir = dir;
+void ElevateCollector::Construct(bool up){
+	this->goUp = up;
 	Requires(Robot::collector);
 }
 // Called just before this Command runs the first time
 void ElevateCollector::Initialize() {
-	Robot::collector->Elevate(dir);
+	if(goUp){
+		Robot::elevator->SetSetpoint(Robot::elevator->GetSetpoint() + .1);
+	}
+	else{
+		Robot::elevator->SetSetpoint(Robot::elevator->GetSetpoint() - .1);
+	}
 }
 // Called repeatedly when this Command is scheduled to run
 void ElevateCollector::Execute() {
