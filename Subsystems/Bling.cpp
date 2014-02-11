fa7e62da8b1676356c4368adea09e44d5adc03eb
Ktrod <Keenan.troddyn@gmail.com>
2014-02-10 20:26:40 -0500

    Added two new functions for blinking and solid lights

--- Subsystems/Bling.cpp
+++ Subsystems/Bling.cpp
@@ -35,20 +35,41 @@ void Bling::Execute()
 	//printf(".\n");
 	switch (pattern){
 	case OFF:
+		clearStrip();
+		for(i = 0; i < 64; i++){
+			dispColor(128, 128, 128);
+		}
 		break;
 	case BLUE_SOLID:
+		clearStrip();
+			for(i = 0; i < 64; i++){
+				dispColor(128, 128, 128);
+			}
 		break;
 	case BLUE_BLINKING:
 		break;
 	case GREEN_SOLID:
+		clearStrip();
+			for(i = 0; i < 64; i++){
+				dispColor(128, 128, 128);
+			}
 		break;
 	case GREEN_BLINKING:
+		clearStrip();
+		
+		
 		break;
 	case RAINBOW_EXPLOSION:
+		clearStrip();
+		rainbowExplosion();
 		break;
 	case RED_BLINKING:
 		break;
 	case PURPLE_SOLID:
+		clearStrip();
+			for(i = 0; i < 64; i++){
+				dispColor(128, 128, 128);
+			}
 		break;
 	case PURPLE_BLINKING:
 		break;
@@ -169,4 +190,30 @@ void Bling::clearStrip() {
     dispClear();
   }
   magicFunction();
+}
+void Bling::makeItBlink(int r, int g, int b)
+{
+  for(int j = 0; j < 500; j++)
+  { 
+    for(int i = 0; i <= 64; i++){
+       dispColor(r, g, b);
+      
+    }
+     setStrip();
+    
+    
+    delay(25);
+     for(int k = 0; k <= 64; k++)
+     {
+       dispColor(0, 0, 0);
+      
+    }
+     setStrip();
+    delay(25);
+  }
+}
+void Bling::setCoolor(int r, int g, int b){
+  for(int i = 0; i < 64; i++){
+    dispColor(r, g, b);
+  }
 }
