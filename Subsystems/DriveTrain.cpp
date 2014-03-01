be6c7e2496b628b33f1cb9556268514070cfcfd6
Evin Ugur <evinoog96@gmail.com>
2014-03-01 12:50:53 -0500

    removed soft limits TEST THIS AND MERGE

--- Subsystems/DriveTrain.cpp
+++ Subsystems/DriveTrain.cpp
@@ -69,22 +69,7 @@ void DriveTrain::MecanumDriveAction(float joystickX, float joystickY, float joys
 	else newTwist = joystickTwist;
 
 	//lower speed / current draw of high gear
-	if (!Robot::shifter->IsLowGear())
-	{	
-		if (newY >= 0)
-			newY = std::min(newY, 0.8f);
-		else
-			newY = std::max(newY, -0.8f);
-		//get 80% * TWIST_CONSTANT below
-		if (newTwist >= 0)
-			newTwist = std::min(newTwist,0.8f);
-		else
-			newTwist = std::max(newTwist,-0.8f);
-		if (newX >= 0)
-			newX = std::min(newX,0.5f);
-		else
-			newX = std::max(newX,-0.5f);
-	}
+	
 		
 	oldX = newX;
 	oldY = newY;
